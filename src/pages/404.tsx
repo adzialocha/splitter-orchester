cf6488dd333400c9fc4a83e9bb4c5aa724dce472
Andreas Dzialocha <x12@adz.garden>
2021-06-11 01:37:08 +0200

    Fix navigation for error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
 
+import type { GetStaticProps } from 'next';
+import type { Post } from 'sanity-schema';
+
+import { getNavigation } from '~/helpers';
+
 import Container from '~/components/Container';
 import Layout from '~/components/Layout';
 import Paragraph from '~/components/Paragraph';
 
-export default function NotFoundError(): JSX.Element {
+type Props = {
+  mainNavigation: Post[];
+  footerNavigation: Post[];
+};
+
+export default function NotFoundError({
+  mainNavigation,
+  footerNavigation,
+}: Props): JSX.Element {
   return (
-    <Layout>
+    <Layout mainNavigation={mainNavigation} footerNavigation={footerNavigation}>
       <Container>
         <Paragraph>404</Paragraph>
       </Container>
     </Layout>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const mainNavigation = await getNavigation('mainNavigation');
+  const footerNavigation = await getNavigation('footerNavigation');
+
+  return {
+    props: {
+      mainNavigation,
+      footerNavigation,
+    },
+  };
+};
