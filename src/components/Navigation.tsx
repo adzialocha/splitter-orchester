746f1519e522e697cd4a08052b54520d27256fad
Andreas Dzialocha <x12@adz.garden>
2021-06-26 14:28:28 +0200

    Make linter happy, add maxWidth to navigation items

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -31,24 +31,25 @@ export default function NavigationHeader({ items }: Props): JSX.Element {
 function NavigationHeaderTriangle({ onClick }): JSX.Element {
   return (
     <button className="p-5 focus:outline-none" onClick={onClick}>
-      <span className="absolute transform rotate-60 -left-2 bottom-11">
+      <span className="absolute bottom-11 -left-2 transform rotate-60">
         Navigation
       </span>
-      <div className="clip-arrow-down w-28 h-24 bg-white" />
+      <div className="w-28 h-24 bg-white clip-arrow-down" />
     </button>
   );
 }
 
 function NavigationHeaderMenu({ items, onClick }): JSX.Element {
   return (
-    <ul className="absolute -left-16 -bottom-56 clip-arrow-up bg-white text-gray text-center py-28 w-96 h-80">
-      <NavigationHeaderItem href="/" onClick={onClick}>
+    <ul className="absolute -bottom-56 -left-16 py-28 w-96 h-80 text-center text-gray bg-white clip-arrow-up">
+      <NavigationHeaderItem href="/" index={0} onClick={onClick}>
         Home
       </NavigationHeaderItem>
-      {items.map((item) => {
+      {items.map((item, index) => {
         return (
           <NavigationHeaderItem
             href={`/${item.slug}`}
+            index={index + 1}
             key={item.slug}
             onClick={onClick}
           >
@@ -60,9 +61,19 @@ function NavigationHeaderMenu({ items, onClick }): JSX.Element {
   );
 }
 
-function NavigationHeaderItem({ children, href, onClick }): JSX.Element {
+function NavigationHeaderItem({ children, href, onClick, index }): JSX.Element {
+  // Since the navigation is a weird triangle we make sure the text of each
+  // navigation item is not wider than the border of the shape
+  const minMaxWidth = 130;
+  const extraWidthPerStep = 28;
+  const maxWidth = minMaxWidth + index * extraWidthPerStep;
+
   return (
-    <li className="max-w-xs m-auto" onClick={onClick}>
+    <li
+      className="overflow-hidden m-auto overflow-ellipsis"
+      style={{ maxWidth }}
+      onClick={onClick}
+    >
       <Link href={href}>{children}</Link>
     </li>
   );
