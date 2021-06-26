128235a300d784328812f543accf958bb31d2536
Andreas Dzialocha <x12@adz.garden>
2021-06-26 18:03:29 +0200

    Always fix footer to bottom

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,6 +4,7 @@ import { AudioPlayerProvider } from '~/state';
 import type { Navigations } from '~/types';
 
 import AudioPlayer from '~/components/AudioPlayer';
+import Box from '~/components/Box';
 import Container from '~/components/Container';
 import Footer from '~/components/Footer';
 import HeaderPage from '~/components/HeaderPage';
@@ -16,10 +17,10 @@ type Props = {
 
 export default function Layout({ children, navigations }: Props): JSX.Element {
   return (
-    <>
+    <Box className="flex flex-col h-screen">
       <HeaderPage items={navigations.main} />
       <AudioPlayerProvider>
-        <main className="pt-2 sm:pt-5">{children}</main>
+        <main className="flex-grow pt-2 sm:pt-5">{children}</main>
         <AudioPlayer />
       </AudioPlayerProvider>
       <Footer className="py-5 sm:py-10">
@@ -27,6 +28,6 @@ export default function Layout({ children, navigations }: Props): JSX.Element {
           <NavigationFooter items={navigations.footer} />
         </Container>
       </Footer>
-    </>
+    </Box>
   );
 }
