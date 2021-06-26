ac527ed195bc23fe0f4f04ac40679ff4fa31a810
Andreas Dzialocha <x12@adz.garden>
2021-06-26 15:51:29 +0200

    Indicate loading progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,37 @@
 import Head from 'next/head';
-import React from 'react';
+import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
+import React, { useEffect } from 'react';
 
 import type { AppProps } from 'next/app';
 
 import '../styles/globals.css';
+import '../styles/nprogress.css';
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter();
+
+  // Use `nprogress` to indicate loading state of routes
+  useEffect(() => {
+    const handleStart = () => {
+      NProgress.start();
+    };
+
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
+
   return (
     <>
       <Head>
